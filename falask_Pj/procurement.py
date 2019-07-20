b0a0719625487058de4651cd1d1b08f1ebe70a35
Xi-Jiawei <veeveev@outlook.com>
2019-07-21 00:08:31 +0800

    更新： 添加采购模块权限。

--- falask_Pj/procurement.py
+++ falask_Pj/procurement.py
@@ -11,33 +11,44 @@ procurement_app=Blueprint('procurement',__name__)
 # 成品管理
 @procurement_app.route('/procurement_history', methods=['GET', 'POST'])
 def procurement_history():
-    # addProductForm = AddProductForm()
-    # print(session)
-    # print(session.keys())
-    # print(session.get('username'))
-    # if session.get('username'):
-    #     username = session['username']
-    #     authority = login_Authority(username)
-    #     procurementList = select_procurement()
-    #     return render_template('procurement.html', form=addProductForm,authority=authority[2], procurementList=procurementList)
-    # else:
-    #     return render_template('test_fail.html')
     addProductForm = AddProductForm()
-    #username = session['username']
-    username = '习佳威'
-    authority = login_Authority(username)
-    procurementResult = select_procurement()
-    procurement_history=[[0 for key in range(7)] for key in range(len(procurementResult))]
-    for i in range(len(procurementResult)):
-        procurement_history[i][0]=procurementResult[i][0]
-        procurement_history[i][1]=procurementResult[i][1].replace(',','/')
-        procurement_history[i][2]=procurementResult[i][2].replace(',','/')
-        procurement_history[i][3]=procurementResult[i][3].replace(',','/')
-        procurement_history[i][4]=procurementResult[i][4][:procurementResult[i][4].index(',')]
-        procurement_history[i][5]=procurementResult[i][5][:procurementResult[i][5].index(',')]
-        procurement_history[i][6]=procurementResult[i][6][:procurementResult[i][6].index(',')]
-    return render_template('procurement_history.html', form=addProductForm, procurement_history=procurement_history,
-                           authority=authority[2])
+    print(session)
+    print(session.keys())
+    print(session.get('username'))
+    if request.method == 'GET':
+        if session.get('username'):
+            username = session['username']
+            authority = login_Authority(username)
+            if authority[2]=="3" or authority[2]=="2":
+                procurementResult = select_procurement()
+                procurement_history = [[0 for key in range(7)] for key in range(len(procurementResult))]
+                for i in range(len(procurementResult)):
+                    procurement_history[i][0] = procurementResult[i][0]
+                    procurement_history[i][1] = procurementResult[i][1].replace(',', '/')
+                    procurement_history[i][2] = procurementResult[i][2].replace(',', '/')
+                    procurement_history[i][3] = procurementResult[i][3].replace(',', '/')
+                    # procurement_history[i][4] = procurementResult[i][4][:procurementResult[i][4].index(',')]
+                    # procurement_history[i][5] = procurementResult[i][5][:procurementResult[i][5].index(',')]
+                    # procurement_history[i][6] = procurementResult[i][6][:procurementResult[i][6].index(',')]
+                    if not procurementResult[i][4].find(',') == -1:
+                        procurement_history[i][4] = procurementResult[i][4][:procurementResult[i][4].find(',')]
+                    else:
+                        procurement_history[i][4] = procurementResult[i][4]
+                    if not procurementResult[i][5].find(',') == -1:
+                        procurement_history[i][5] = procurementResult[i][5][:procurementResult[i][5].find(',')]
+                    else:
+                        procurement_history[i][5] = procurementResult[i][5]
+                    if not procurementResult[i][6].find(',') == -1:
+                        procurement_history[i][6] = procurementResult[i][6][:procurementResult[i][6].find(',')]
+                    else:
+                        procurement_history[i][6] = procurementResult[i][6]
+                return render_template('procurement_history.html', form=addProductForm,
+                                       procurement_history=procurement_history,
+                                       authority=authority[2])
+            else:
+                return render_template('procurement.html', form=addProductForm, authority=authority[2])
+        else:
+            return render_template('test_fail.html')
 
 # xijiawei
 # 成品管理
@@ -293,69 +304,67 @@ def productCodeByType():
 #
 @procurement_app.route('/procurement', methods=['GET', 'POST'])
 def procurement():
-    # addProductForm = AddProductForm()
-    # print(session)
-    # print(session.keys())
-    # print(session.get('username'))
-    # if session.get('username'):
-    #     username = session['username']
-    #     authority = login_Authority(username)
-    #     procurementList = select_procurement()
-    #     return render_template('procurement.html', form=addProductForm,authority=authority[2], procurementList=procurementList)
-    # else:
-    #     return render_template('test_fail.html')
-    #if addProductForm.validate_on_submit():
-    if request.method=="POST":
-        # data=request.get_data()
-        # data=json.load(request.get_data(as_text=True))
-        data=request.get_json()
-        productCodeOrTypeInput=data['productCodeOrTypeInput']
-        productCodeOrType=data['productCodeOrType']
-        productNumArr=data['productNumArr']
-        client=data['client']
-        materialArr=data['materialArr']
-        if session.get('username'):
-            entryClerk=session['username']
-        else:
-            entryClerk="admin"
-        entryDate=datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-        if productCodeOrType=="0":
-            productCodeArr=productCodeOrTypeInput
-            if not select_productTypeByCode(productCodeArr[len(productCodeArr)-1]):
-                return jsonify({'ok': False})
+    print(session)
+    print(session.keys())
+    print(session.get('username'))
+    if session.get('username'):
+        username = session['username']
+        authority = login_Authority(username)
+        if request.method == "POST":
+            # data=request.get_data()
+            # data=json.load(request.get_data(as_text=True))
+            data = request.get_json()
+            productCodeOrTypeInput = data['productCodeOrTypeInput']
+            productCodeOrType = data['productCodeOrType']
+            productNumArr = data['productNumArr']
+            client = data['client']
+            materialArr = data['materialArr']
+            if session.get('username'):
+                entryClerk = session['username']
             else:
-                maxid=select_maxid_procurement()
-                if not maxid[0][0]==None:
-                    id=int(maxid[0][0])+1
+                entryClerk = "admin"
+            entryDate = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            if productCodeOrType == "0":
+                productCodeArr = productCodeOrTypeInput
+                if not select_productTypeByCode(productCodeArr[len(productCodeArr) - 1]):
+                    return jsonify({'ok': False})
                 else:
-                    id=0
-                for i in range(len(productCodeArr)):
-                    productType = select_productTypeByCode(productCodeArr[i])
-                    insert_procurement(id,productCodeArr[i], productType[0][0], int(productNumArr[i]), client, entryClerk,
-                                       entryDate)
-        elif productCodeOrType=="1":
-            productTypeArr = productCodeOrTypeInput
-            if not select_productCodeByType(productTypeArr[len(productTypeArr)-1]):
-                return jsonify({'ok': False})
-            else:
-                maxid=select_maxid_procurement()
-                if not maxid[0][0]==None:
-                    id=int(maxid[0][0])+1
+                    maxid = select_maxid_procurement()
+                    if not maxid[0][0] == None:
+                        id = int(maxid[0][0]) + 1
+                    else:
+                        id = 0
+                    for i in range(len(productCodeArr)):
+                        productType = select_productTypeByCode(productCodeArr[i])
+                        insert_procurement(id, productCodeArr[i], productType[0][0], int(productNumArr[i]), client,
+                                           entryClerk,
+                                           entryDate)
+            elif productCodeOrType == "1":
+                productTypeArr = productCodeOrTypeInput
+                if not select_productCodeByType(productTypeArr[len(productTypeArr) - 1]):
+                    return jsonify({'ok': False})
                 else:
-                    id=0
-                for i in range(len(productTypeArr)):
-                    productCode = select_productCodeByType(productTypeArr[i])
-                    insert_procurement(id,productCode[0][0], productTypeArr[i], int(productNumArr[i]), client, entryClerk,
-                                       entryDate)
-        for material in materialArr:
-            print(material['materialCode'])
-            print(int(material['remainderAmount']))
-            update_materialOfInfo(material['materialCode'], int(material['remainderAmount']))
-            materialOfInfo = select_materialOfInfo(material['materialCode'])
-            insert_materialOfInOut(material['materialCode'], '出库', int(material['materialAmount']),
-                                   int(material['remainderAmount']),
-                                   int(material['remainderAmount']) * materialOfInfo[0][3])
-        return jsonify({'ok': True})
-    elif request.method=='GET':
-        addProductForm = AddProductForm()
-        return render_template('procurement.html', form=addProductForm)
+                    maxid = select_maxid_procurement()
+                    if not maxid[0][0] == None:
+                        id = int(maxid[0][0]) + 1
+                    else:
+                        id = 0
+                    for i in range(len(productTypeArr)):
+                        productCode = select_productCodeByType(productTypeArr[i])
+                        insert_procurement(id, productCode[0][0], productTypeArr[i], int(productNumArr[i]), client,
+                                           entryClerk,
+                                           entryDate)
+            for material in materialArr:
+                print(material['materialCode'])
+                print(int(material['remainderAmount']))
+                update_materialOfInfo(material['materialCode'], int(material['remainderAmount']))
+                materialOfInfo = select_materialOfInfo(material['materialCode'])
+                insert_materialOfInOut(material['materialCode'], '出库', int(material['materialAmount']),
+                                       int(material['remainderAmount']),
+                                       int(material['remainderAmount']) * materialOfInfo[0][3])
+            return jsonify({'ok': True})
+        elif request.method == 'GET':
+            addProductForm = AddProductForm()
+            return render_template('procurement.html', form=addProductForm, authority=authority[2])
+    else:
+        return render_template('test_fail.html')
